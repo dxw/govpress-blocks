ae783f930909d624a7eed85344ca6c91ef570f4c
Rob Skilling <rob@dxw.com>
2022-07-28 11:28:57 +0100

    Add block factory class
    I've gone back and forth on the best design pattern to use here, but I
    think this is a good compromise to start off with.
    
    A new block will be created by calling e.g.
    `\Dxw\GovpressBlocks\Factory::create('card')`, which will register all
    the fields etc. necessary for that block (and also return the card
    instance, although there's nothing you can do with that for now).
    
    I'm also enforcing a kind of singleton here, by causing an exception to be
    thrown if you try and new up the same block type twice (which could
    cause ACF-related weirdness). But I'm not using an actual singleton
    pattern, because the factory-type pattern seems more testable.
    
    This should allow us to add new block types by just writing the
    necessary block class, then adding a reference to the `BLOCKS` constant
    on the factory.
    
    An actual block will be added in a subsequent commit.

---
+++ spec/factory.spec.php
@@ -0,0 +1,31 @@
+<?php
+
+use Kahlan\Plugin\Double;
+
+describe('\Dxw\GovPressBlocks\Factory::class', function () {
+    describe('::create()', function () {
+        it('returns an instance of a block if it exists, and throws an exception if you try to create a block twice', function () {
+            foreach (\Dxw\GovPressBlocks\Factory::BLOCKS as $blockName => $className) {
+                $result = \Dxw\GovPressBlocks\Factory::create($blockName, []);
+                expect($result)->toBeAnInstanceOf($className);
+            }
+            // Ideally this would be a separate test
+            // But can't find a way to stop the class persisting across tests
+            $closure = function () {
+                \Dxw\GovPressBlocks\Factory::create('card');
+            };
+            expect($closure)->toThrow(new Exception('GovPressBlock type already created: card'));
+        });
+
+        xit('passes the optional args array to the block constructor', function () {
+            // Can't find a way to create a double that correctly implements the iBlock interface
+        });
+
+        it('throws an exception if you try to new up a card type that does not exist', function () {
+            $closure = function () {
+                \Dxw\GovPressBlocks\Factory::create('foo', []);
+            };
+            expect($closure)->toThrow(new Exception('GovPressBlock type does not exist: foo'));
+        });
+    });
+});
