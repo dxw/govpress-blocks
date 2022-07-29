d75b7cce2d4aad31497aedd7c91118195c8112db
Rob Skilling <rob@dxw.com>
2022-07-29 11:59:11 +0100

    Add basic default card template
    This just includes the link and description fields, as the components
    library example does not include an image.

--- templates/card.php
+++ templates/card.php
@@ -1,2 +1,28 @@
-<h1>Card template</h1>
-<p>To be populated</p>
+<?php
+
+$link = get_field('link');
+
+?>
+
+<article class="card card--full card--default-bg">
+  <div class="card__content">
+    <!-- CONTENT -->
+    <div class="card__text">
+
+      <!-- TITLE -->
+      <h3 class="govuk-heading-m">
+        <a class="card__link" href="<?php echo esc_url($link['url']) ?>" rel="bookmark"><?php echo wp_kses_post($link['title']) ?></a>
+      </h3>
+
+      <?php if (get_field('card_style') === 'full' && get_field('description')) : ?>
+      <!-- DESCRIPTION -->
+      <p class="card__description">
+        <?php echo wp_kses_post(get_field('description')) ?>
+      </p>
+      <?php endif; ?>
+    </div>
+    <!-- /.card__text -->
+    
+  </div>
+  <!-- /.card__content -->
+</article>
